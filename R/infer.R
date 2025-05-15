b406f9860ff9e9ec1adefc0b743a74f0606224ac
Edzai Zvobwo <edzaizvobwo@gmail.com>
2025-05-15 17:14:46 +0200

    Initial commit of impactr package

--- R/infer.R
+++ R/infer.R
@@ -1,5 +1,17 @@
-#' infer module
+#' infer_impact module
 #' @export
-infer <- function() {
-  # TODO
+infer_impact <- function(data, logic_model, method = "causalforest") {
+  if (!requireNamespace("grf", quietly = TRUE)) {
+    stop("Package 'grf' is required for this function. Please install it.")
+  }
+
+  # Example: simple causal forest
+  X <- data[, c("region", "period")]
+  Y <- data$outcome_kpi
+  W <- data$treatment
+
+  cf_model <- grf::causal_forest(X, Y, W)
+  predictions <- predict(cf_model)
+
+  return(predictions)
 }
